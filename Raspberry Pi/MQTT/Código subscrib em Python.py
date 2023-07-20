8d50a96974dbd93b37d8e2da72c85136c70c9e23
Ramon Martins <ramon-bac@hotmail.com>
2023-07-20 19:18:27 -0300

    update mqtt

--- Raspberry Pi/MQTT/Código subscrib em Python.py
+++ Raspberry Pi/MQTT/Código subscrib em Python.py
@@ -1,14 +1,12 @@
 import paho.mqtt.client as paho
 
 def on_message(mosq, obj, msg):
-    print("%-20s %d %s" % (msg.topic, msg.qos, msg.payload))
-    mosq.publish('pong', 'ack', 0)
+    print(f"Payload: {msg.payload.decode('utf8')}")
 
 client = paho.Client()
-
 client.on_message = on_message
 
-client.connect("192.168.0.58", 1883, 60)
+client.connect("localhost", 1883, 60)
 
 client.subscribe("home_artur/sensor", 0)
 
